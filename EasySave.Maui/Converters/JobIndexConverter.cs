92994f64a5a0c6e9f8a8c9cecd221e130a24361f
StephanCESI <stephan.laumel@viacesi.fr>
2025-05-16 16:39:27 +0200

    fix collectionview + add logo

---
+++ EasySave.Maui/Converters/JobIndexConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasySave.Maui.Models;
+
+namespace EasySave.Maui.Converters;
+
+public class JobIndexConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length == 2 && values[0] is IList<BackupJob> jobs && values[1] is BackupJob currentJob)
+        {
+            return jobs.IndexOf(currentJob);
+        }
+        return 0; // Retourne 0 si les valeurs ne sont pas valides
+    }
+
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
