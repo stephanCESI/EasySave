b36975327405aedf5f2cd474f96fdd422cce8857
matthisb <matthis.behra04@gmail.com>
2025-05-26 15:29:44 +0200

    front + socket

---
+++ Customer/Services/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Services
+{
+    internal class Customer
+    {
+        private static Socket ConnectToServer()
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress serverAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint serverEndPoint = new IPEndPoint(serverAddress, 8080);
+
+            try
+            {
+                clientSocket.Connect(serverEndPoint);
+                Console.WriteLine("Connect to server");
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Error connecting to server: " + ex.Message);
+                return null!;
+            }
+
+            return clientSocket;
+        }
+
+
+        private static void ListenToServer(Socket client)
+        {
+
+        }
+
+        private static void Disconnect(Socket socket)
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+
+        }
+    }
+}
---
+++ EasySave.Maui/Services/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EasySave.Maui.Services
+{
+    internal class Server
+    {
+        private static Socket StartServer()
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 8080));
+            serverSocket.Listen(10);
+            Console.WriteLine("Server started. Waiting for connections...");
+            return serverSocket;
+        }
+
+        private static Socket AcceptConnection(Socket socket)
+        {
+            Socket clientSocket = socket.Accept();
+           
+            IPEndPoint clientEndPoint = (IPEndPoint)clientSocket.RemoteEndPoint ;
+            Console.WriteLine($"Client connected from {clientEndPoint?.Address} : {clientEndPoint?.Port}");
+            return clientSocket;
+        }
+
+        private static void ListenToClient(Socket client)
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead = client.Receive(buffer);
+            string message = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        }
+
+        private static void Disconnect(Socket socket)
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+    }
+}
